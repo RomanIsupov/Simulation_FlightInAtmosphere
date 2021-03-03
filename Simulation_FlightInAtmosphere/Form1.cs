fafbd58cf672523e636535511668712e27f88237
RomanIsupov <romisup@gmail.com>
2021-03-03 19:10:30 +0800

    Add time indicator, pause button.

--- Simulation_FlightInAtmosphere/Form1.cs
+++ Simulation_FlightInAtmosphere/Form1.cs
@@ -15,6 +15,9 @@ namespace Simulation_FlightInAtmosphere
     {
         private Flight.Flight flight = new Flight.Flight();
         private Point current;
+        private bool isStarted = false;
+        private double t = 0D;
+        private const double dt = 0.01D;
 
         public Form1()
         {
@@ -23,21 +26,40 @@ namespace Simulation_FlightInAtmosphere
 
         private void btStart_Click(object sender, EventArgs e)
         {
+            if (isStarted)
+            {
+                timer1.Start();
+                return;
+            }
+
             flight.SetParameters((double)edAngle.Value, (double)edSpeed.Value, (double)edHeight.Value, (double)edWeight.Value, (double)edSquare.Value);
 
             chart1.Series[0].Points.Clear();
             chart1.Series[0].Points.AddXY(0, (double)edHeight.Value);
+            isStarted = true;
             if (!timer1.Enabled)
             {
                 timer1.Start();
             }
+            t = 0D;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             current = flight.ToNextState();
             chart1.Series[0].Points.AddXY(current.x, current.y);
+            t += dt;
+            labTime.Text = $"Time: {Math.Round(t, 2)}";
             if (current.y <= 0)
+            {
+                timer1.Stop();
+                isStarted = false;
+            }
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
             {
                 timer1.Stop();
             }
